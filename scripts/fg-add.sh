af7ea93ef0caf90b3280383e2e0679bad415a153
mulmeyun <mulmeyun@godspeed.moe>
2023-08-19 11:50:23 -0400

    cped img-wmark to fg-add.sh

---
+++ scripts/fg-add.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# depends on
+# - imagemagick
+
+# escape bad filenames
+IFS="$(printf '\n\t')"
+controlchars="$(printf '*[\001-\037\177]*')"
+
+# bash array
+files=( $(find . -type f ! -name "$controlchars" -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg") )
+
+#echo "$files"
+
+i=$(echo "${#files[@]}" | bc)
+
+for ((x=0; x < i; x++)); do
+	echo "Processing ${files[x]}"
+	convert -composite -gravity center "${files[x]}" logo.png "${files[x]}-logo.png"
+done
