692df58913d5a383d44bbdfe02325fbd689a864d
alepaillas <alepaillas@gmail.com>
2023-08-28 15:11:31 -0400

    good img-enhance

--- scripts/img-enhance.sh
+++ scripts/img-enhance.sh
@@ -7,16 +7,22 @@
 IFS="$(printf '\n\t')"
 controlchars="$(printf '*[\001-\037\177]*')"
 
+wd="$(pwd)"
+#echo "$wd"
+
 # bash array
-files=( $(find . -type f ! -name "$controlchars" -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg") )
+files=( $(find . -maxdepth 1 -type f ! -name "$controlchars" -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg") )
 
 #echo "$files"
 
 i=$(echo "${#files[@]}" | bc)
 
+mkdir "$wd"/edited-image
+
 for ((x=0; x < i; x++)); do
 	echo "Processing ${files[x]}"
-	#convert "${files[x]}" -resize 1500x1500 "${files[x]}-1500px.png"
-	convert -enhance -equalize -contrast "${files[x]}" "${files[x]}"
+	#convert -enhance -equalize -contrast "${files[x]}" "${files[x]}"
 	#convert -auto-gamma -auto-level -normalize "${files[x]}" "${files[x]}"
+	#convert -auto-level "${files[x]}" "$wd"/edited-image/"${files[x]}"
+	convert "${files[x]}" -channel green -evaluate multiply 0.90 +channel -normalize -auto-level -fill khaki1 -colorize 35% -brightness-contrast -10x10 "$wd"/edited-image/"${files[x]}"
 done
