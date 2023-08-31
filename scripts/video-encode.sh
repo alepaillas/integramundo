a10fcb30c43aff591d60a092e6b64929227fdb10
alepaillas <alepaillas@gmail.com>
2023-08-31 13:26:02 -0400

    encode with audio and images

--- scripts/video-encode.sh
+++ scripts/video-encode.sh
@@ -10,7 +10,8 @@ CONTAINER_PARAMS="-movflags faststart"
 # You need to adjust the GOP length to fit your source video.
 # 60 fps -> -g 30
 # 23.976 (24000/1001) -> -g 24000/1001/2  (???) <- plz comment
-# 29.970 (30000/1001) -> -g (30000/1001/2) = 14,985
+# 29.970 (30000/1001) -> -g (30000/1001/2) = 14.985
 
 # ffmpeg -i "$1" $AUDIO_PARAMS $VIDEO_PARAMS $CONTAINER_PARAMS "$2"
-ffmpeg -i "$1" $AUDIO_PARAMS $VIDEO_PARAMS -vf "hqdn3d" $CONTAINER_PARAMS "$2"
+# ffmpeg -i "$1" $AUDIO_PARAMS $VIDEO_PARAMS -vf "hqdn3d" $CONTAINER_PARAMS "$2"
+ffmpeg -framerate 29.97 -pattern_type glob -i "*.png" -i "$1" $AUDIO_PARAMS -shortest $VIDEO_PARAMS -vf "hqdn3d" $CONTAINER_PARAMS "$2"
