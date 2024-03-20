2213a47dc2ee4c2bdd1a076c46013d43995f5a50
alepaillas <139644184+alepaillas@users.noreply.github.com>
2024-03-20 09:50:22 -0300

    format y movi get request dentro de la logica del rfid

--- arduino/control-asistencia-odoo/main.ino
+++ arduino/control-asistencia-odoo/main.ino
@@ -1,22 +1,21 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <MFRC522.h>
-
-#include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <MFRC522.h>
+#include <SPI.h>
+#include <WiFiClientSecure.h>
 
-const int D0 = 16; //GPIO16 - WAKE UP //buzzer
-const int D1 = 5;  //GPIO5
-const int D2 = 4;  //GPIO4
-const int D3 = 0;  //GPIO0
-const int D4 = 2;  //GPIO2 - TXD1
-const int D5 = 14; //GPIO14 - HSCLK
-const int D6 = 12; //GPIO12 - HMISO
-const int D7 = 13; //GPIO13 - HMOSI - RXD2
-const int D8 = 15; //GPIO15 - HCS   - TXD2
-const int RX = 3;  //GPIO3 - RXD0 
-const int TX = 1;  //GPIO1 - TXD0
+const int D0 = 16; // GPIO16 - WAKE UP //buzzer
+const int D1 = 5;  // GPIO5
+const int D2 = 4;  // GPIO4
+const int D3 = 0;  // GPIO0
+const int D4 = 2;  // GPIO2 - TXD1
+const int D5 = 14; // GPIO14 - HSCLK
+const int D6 = 12; // GPIO12 - HMISO
+const int D7 = 13; // GPIO13 - HMOSI - RXD2
+const int D8 = 15; // GPIO15 - HCS   - TXD2
+const int RX = 3;  // GPIO3 - RXD0
+const int TX = 1;  // GPIO1 - TXD0
 const int S3 = 10;
 const int S2 = 9;
 
@@ -37,14 +36,14 @@ MFRC522::MIFARE_Key key;
 // Init array that will store new NUID
 byte nuidPICC[4];
 
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 String DatoHex;
 const String UserReg_1 = "F3073CF6";
 const String UserReg_2 = "";
 const String UserReg_3 = "";
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-const char IRG_Root_X1 [] PROGMEM = R"CERT(
+const char IRG_Root_X1[] PROGMEM = R"CERT(
 -----BEGIN CERTIFICATE-----
 MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ
 RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD
@@ -68,19 +67,19 @@ R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp
 -----END CERTIFICATE-----
 )CERT";
 
-const char* ssid = "Integramundo";
-const char* password = "integramundo2023";
+const char *ssid = "Integramundo";
+const char *password = "integramundo2023";
 
 X509List cert(IRG_Root_X1);
 
-void parpadearLed (int pin, int ms) {
+void parpadearLed(int pin, int ms) {
   digitalWrite(pin, HIGH);
   delay(ms);
   digitalWrite(pin, LOW);
   delay(ms);
 }
 
-void pulsarBuzzer (int buzzer, int ms) {
+void pulsarBuzzer(int buzzer, int ms) {
   digitalWrite(buzzer, HIGH);
   delay(ms);
   digitalWrite(buzzer, LOW);
@@ -99,38 +98,50 @@ void cambiarModo(int led1, int led2) {
   }
 }
 
+String printHex(byte *buffer, byte bufferSize) {
+  String DatoHexAux = "";
+  for (byte i = 0; i < bufferSize; i++) {
+    if (buffer[i] < 0x10) {
+      DatoHexAux = DatoHexAux + "0";
+      DatoHexAux = DatoHexAux + String(buffer[i], HEX);
+    } else {
+      DatoHexAux = DatoHexAux + String(buffer[i], HEX);
+    }
+  }
 
-void setup() 
-{
+  for (int i = 0; i < DatoHexAux.length(); i++) {
+    DatoHexAux[i] = toupper(DatoHexAux[i]);
+  }
+  return DatoHexAux;
+}
+
+void setup() {
   pinMode(BOTON, INPUT_PULLUP);
   pinMode(LED_VERDE, OUTPUT);
   pinMode(LED_ROJO, OUTPUT);
   pinMode(BUZZER, OUTPUT);
- 
 
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
 
-   Serial.begin(115200);
-   SPI.begin(); // Init SPI bus
-   rfid.PCD_Init(); // Init MFRC522
-   Serial.println();
-   Serial.print(F("Reader :"));
-   rfid.PCD_DumpVersionToSerial();
-   for (byte i = 0; i < 6; i++) {
-     key.keyByte[i] = 0xFF;
-   } 
-   DatoHex = printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
-   Serial.println();
-   Serial.println();
-   Serial.println("Iniciando el Programa");
- 
-Serial.begin(115200);
-  //Serial.setDebugOutput(true);
+  SPI.begin();     // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522
+  Serial.println();
+  Serial.print(F("Reader :"));
+  rfid.PCD_DumpVersionToSerial();
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  DatoHex = printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+  Serial.println();
+  Serial.println();
+  Serial.println("Iniciando el Programa");
 
   Serial.println();
   Serial.println();
   Serial.println();
 
-  //Connect to Wi-Fi
+  // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
@@ -154,13 +165,13 @@ Serial.begin(115200);
   gmtime_r(&now, &timeinfo);
   Serial.print("Current time: ");
   Serial.print(asctime(&timeinfo));
-
 }
 
-void loop() 
-{
-digitalWrite(BUZZER, HIGH);
-    bool estadoBoton = digitalRead(BOTON);
+void loop() {
+  // partir con el buzzer apagado
+  digitalWrite(BUZZER, HIGH);
+
+  bool estadoBoton = digitalRead(BOTON);
   unsigned long tiempoActual = millis();
 
   // Verifica si ha pasado suficiente tiempo desde la última pulsación
@@ -174,111 +185,102 @@ digitalWrite(BUZZER, HIGH);
     estadoAnteriorBoton = estadoBoton;
   }
 
-     // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-     if ( ! rfid.PICC_IsNewCardPresent()){return;}
-     
-     // Verify if the NUID has been readed
-     if ( ! rfid.PICC_ReadCardSerial()){return;}
-     
-     Serial.print(F("PICC type: "));
-     MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-     Serial.println(rfid.PICC_GetTypeName(piccType));
-     // Check is the PICC of Classic MIFARE type
-     if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K)
-     {
-       Serial.println("Su Tarjeta no es del tipo MIFARE Classic.");
-       return;
-     }
-     
-     if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3] )
-     {
-       Serial.println("Se ha detectado una nueva tarjeta.");
-       
-       // Store NUID into nuidPICC array
-       for (byte i = 0; i < 4; i++) {nuidPICC[i] = rfid.uid.uidByte[i];}
-    
-       DatoHex = printHex(rfid.uid.uidByte, rfid.uid.size);
-       Serial.print("Codigo Tarjeta: "); Serial.println(DatoHex);
-    
-       if(UserReg_1 == DatoHex)
-       {
-        Serial.println("USUARIO 1 - PUEDE INGRESAR");
-        pulsarBuzzer ( BUZZER, 300);   
-       }
-       else if(UserReg_2 == DatoHex)
-       {
-        Serial.println("USUARIO 2 - PUEDE INGRESAR");
-       }
-       else if(UserReg_3 == DatoHex)
-       {
-        Serial.println("USUARIO 3 - PUEDE INGRESAR");
-       }
-       else
-       {
-        Serial.println("NO ESTA REGISTRADO - PROHIBIDO EL INGRESO");
-        pulsarBuzzer ( BUZZER, 150);  
-        pulsarBuzzer ( BUZZER, 150);  
-       }  
-       Serial.println();
-     }
-     else 
-     {
-      Serial.println("Tarjeta leida previamente");
-     }
-     // Halt PICC
-     rfid.PICC_HaltA();
-     // Stop encryption on PCD
-     rfid.PCD_StopCrypto1();
-
-WiFiClientSecure client;
-
-  if ((WiFi.status() == WL_CONNECTED)) {
-
-    client.setTrustAnchors(&cert);
-
-    HTTPClient https;
-
-    Serial.print("[HTTPS] begin...\n");
-    if (https.begin(client, "https://flask-asistencia-odoo.onrender.com/hello/paula")) {  // HTTPS
-
-      Serial.print("[HTTPS] GET...\n");
-      int httpCode = https.GET();
-
-      if (httpCode > 0) {
-        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = https.getString();
-          Serial.println(payload);
-        }
-      } else {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
-      }
-
-      https.end();
-    } else {
-      Serial.printf("[HTTPS] Unable to connect\n");
-    }
+  // Reset the loop if no new card present on the sensor/reader. This saves the
+  // entire process when idle.
+  if (!rfid.PICC_IsNewCardPresent()) {
+    return;
   }
 
-  Serial.println();
-  Serial.println("Waiting 2min before the next round...");
-  delay(10000);     
-}
+  // Verify if the NUID has been readed
+  if (!rfid.PICC_ReadCardSerial()) {
+    return;
+  }
 
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println("Su Tarjeta no es del tipo MIFARE Classic.");
+    return;
+  }
 
-String printHex(byte *buffer, byte bufferSize)
-{  
-   String DatoHexAux = "";
-   for (byte i = 0; i < bufferSize; i++) 
-   {
-       if (buffer[i] < 0x10)
-       {
-        DatoHexAux = DatoHexAux + "0";
-        DatoHexAux = DatoHexAux + String(buffer[i], HEX);  
-       }
-       else { DatoHexAux = DatoHexAux + String(buffer[i], HEX); }
-   }
-   
-   for (int i = 0; i < DatoHexAux.length(); i++) {DatoHexAux[i] = toupper(DatoHexAux[i]);}
-   return DatoHexAux;
+  if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+      rfid.uid.uidByte[1] != nuidPICC[1] ||
+      rfid.uid.uidByte[2] != nuidPICC[2] ||
+      rfid.uid.uidByte[3] != nuidPICC[3]) {
+    Serial.println("Se ha detectado una nueva tarjeta.");
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+
+    DatoHex = printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.print("Codigo Tarjeta: ");
+    Serial.println(DatoHex);
+
+    if (UserReg_1 == DatoHex) {
+      Serial.println("USUARIO 1 - PUEDE INGRESAR");
+      pulsarBuzzer(BUZZER, 300);
+
+      WiFiClientSecure client;
+
+      if ((WiFi.status() == WL_CONNECTED)) {
+
+        client.setTrustAnchors(&cert);
+
+        HTTPClient https;
+
+        Serial.print("[HTTPS] begin...\n");
+        if (https.begin(client,
+                        "https://flask-asistencia-odoo.onrender.com/hello/"
+                        "paula")) { // HTTPS
+
+          Serial.print("[HTTPS] GET...\n");
+          int httpCode = https.GET();
+
+          if (httpCode > 0) {
+            Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+            if (httpCode == HTTP_CODE_OK ||
+                httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+              String payload = https.getString();
+              Serial.println(payload);
+            }
+
+            if (httpCode == HTTP_CODE_OK {
+                  pulsarBuzzer(BUZZER, 300);
+                  pulsarBuzzer(BUZZER, 300);
+                })
+          } else {
+            Serial.printf("[HTTPS] GET... failed, error: %s\n",
+                          https.errorToString(httpCode).c_str());
+            pulsarBuzzer(BUZZER, 1000);
+          }
+
+          https.end();
+        } else {
+          Serial.printf("[HTTPS] Unable to connect\n");
+        }
+      }
+    } else if (UserReg_2 == DatoHex) {
+      Serial.println("USUARIO 2 - PUEDE INGRESAR");
+    } else if (UserReg_3 == DatoHex) {
+      Serial.println("USUARIO 3 - PUEDE INGRESAR");
+    } else {
+      Serial.println("NO ESTA REGISTRADO - PROHIBIDO EL INGRESO");
+      pulsarBuzzer(BUZZER, 150);
+      pulsarBuzzer(BUZZER, 150);
+      pulsarBuzzer(BUZZER, 150);
+    }
+    Serial.println();
+  } else {
+    Serial.println("Tarjeta leida previamente");
+  }
+  // Halt PICC
+  rfid.PICC_HaltA();
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
 }
